73ae318d77cd917d90b5e771f55391af8ca43165
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-11-03 00:26:37 -0300

    feat: :sparkles: implement adDataSchema for Joi validation

---
+++ src/services/schemas.js
@@ -0,0 +1,23 @@
+/*
+  Material consultado sobre Joi
+  https://www.digitalocean.com/community/tutorials/how-to-use-joi-for-node-api-schema-validation
+  https://joi.dev/api/?v=17.4.2#general-usage
+*/
+const Joi = require('joi');
+
+const id = Joi.number().integer().positive();
+const text = Joi.string();
+
+const adDataSchema = Joi.object().keys({
+  id,
+  marca: text.max(45).required(),
+  modelo: text.max(45).required(),
+  versao: text.max(45).required(),
+  ano: Joi.number.integer().positive().required(),
+  quilometragem: Joi.number.integer().positive().required(),
+  observacao: text.required(),
+});
+
+module.exports = {
+  adDataSchema,
+};
