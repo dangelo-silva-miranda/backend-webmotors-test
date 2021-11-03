774ad9cdcbea9dfb2b2c1141d365664df0dbd275
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-11-03 01:10:11 -0300

    feat: :sparkles: check ad data using adDataSchema and Joi

--- src/services/adService.js
+++ src/services/adService.js
@@ -1,9 +1,15 @@
 const { StatusCodes } = require('http-status-codes');
 
 const { TbAnuncioWebmotors } = require('../models');
-// const { adDataSchema } = require('./schemas');
+const { adDataSchema } = require('./schemas');
 
 const createAd = async ({ marca, modelo, versao, ano, quilometragem, observacao }) => {
+  const { error } = adDataSchema.validate({ marca, modelo, versao, ano, quilometragem, observacao });
+  if (error) { // error.isJoi indentifica se o erro foi do tipo Joi
+    const { message } = error.details[0];    
+    return { code: StatusCodes.BAD_REQUEST, message };
+  }
+
   const { dataValues: ad } = await TbAnuncioWebmotors.create({ 
     marca, modelo, versao, ano, quilometragem, observacao, 
   });
