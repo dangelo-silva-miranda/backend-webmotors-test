b5ac4584c990b74c52bcfa8b7c60fbeab2b0d371
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-11-02 22:21:56 -0300

    feat: :wrench: create a seed file in seeders folder

---
+++ src/database/seeders/20211103005625-tb_anunciowebmotors.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('tb_AnuncioWebmotors',
+      [{
+        id: 1,
+        marca:'Honda',
+        modelo:"Fit",
+        versao:"1.4 LXL 16V FLEX 4P AUTOMÁTICO",
+        ano:2018,
+        quilometragem:0,
+        observacao:''
+      },
+      {
+        id: 2,
+        marca:'Mitsubishi',
+        modelo:'Lancer',
+        versao:"2.0 EVO 4P AUTOMÁTICO",
+        ano:2012,
+        quilometragem:0,
+        observacao:''
+      },
+      ], { timestamps: false });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+  await queryInterface.bulkDelete('tb_AnuncioWebmotors', null, {});
+  }
+};
